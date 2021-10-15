4d46987a4cf63789af4be8057f6c13bde54a7609
Ryan Thorpe <ryvthorpe@gmail.com>
2021-10-15 17:10:53 -0400

    adjust tail in law model

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import os.path as op
+
+import hnn_core
+from hnn_core import read_params, jones_2009_model
+
 def add_law_beta_drives(net, beta_start, strength=1.0):
     # Distal Drive
     weights_ampa_d1 = {'L2_basket': 0.00032 * strength, 'L2_pyramidal': 0.00008,
@@ -18,7 +23,7 @@ def add_law_beta_drives(net, beta_start, strength=1.0):
 
     net.add_bursty_drive(
         'beta_prox', tstart=beta_start, tstart_std=0., tstop=beta_start + 50.,
-        burst_rate=1., burst_std=20., numspikes=2, spike_isi=10, n_drive_cells=10,
+        burst_rate=1., burst_std=30., numspikes=2, spike_isi=10, n_drive_cells=10,
         location='proximal', weights_ampa=weights_ampa_p1,
         synaptic_delays=syn_delays_p1, event_seed=20)
 
@@ -51,10 +56,85 @@ def add_supra_beta_drives(net, beta_start, strength=1.0):
         synaptic_delays=synaptic_delays_prox, event_seed=4)
 
 def rescale_pyr_morph(net, cell_types, compartment_prop, scaling_factor, omit_compartment=None):
-    if omit_compartment is not None:
+    if omit_compartment is None:
         omit_compartment = list()
     for cell_type in cell_types:
         for section_name in net.cell_types[cell_type].sections:
             if section_name not in omit_compartment:
                 val = getattr(net.cell_types[cell_type].sections[section_name], compartment_prop)
                 setattr(net.cell_types[cell_type].sections[section_name], '_' + compartment_prop, val * scaling_factor)
+                
+def law_2021_model():
+    """Instantiate the beta modulated ERP network model.
+    Returns
+    -------
+    net : Instance of Network object
+        Network object used to store the model used in
+        Law et al. 2021.
+    See Also
+    --------
+    jones_2009_model
+    Notes
+    -----
+    Model reproduces results from Law et al. 2021
+    This model differs from the default network model in several
+    parameters including
+    1) Increased GABAb time constants on L2/L5 pyramidal cells
+    2) Decrease L5_pyramidal -> L5_pyramidal nmda weight
+    3) Modified L5_basket -> L5_pyramidal inhibition weights
+    4) Removal of L5 pyramidal somatic and basal dendrite calcium channels
+    5) Replace L2_basket -> L5_pyramidal GABAa connection with GABAb
+    6) Addition of L5_basket -> L5_pyramidal distal connection
+    """
+
+    hnn_core_root = op.dirname(hnn_core.__file__)
+    params_fname = op.join(hnn_core_root, 'param', 'default.json')
+    params = read_params(params_fname)
+
+    net = jones_2009_model(params)
+
+    # Update biophysics (increase gabab duration of inhibition)
+    net.cell_types['L2_pyramidal'].synapses['gabab']['tau1'] = 45.0
+    net.cell_types['L2_pyramidal'].synapses['gabab']['tau2'] = 200.0
+    net.cell_types['L5_pyramidal'].synapses['gabab']['tau1'] = 45.0
+    net.cell_types['L5_pyramidal'].synapses['gabab']['tau2'] = 200.0
+
+    # Decrease L5_pyramidal -> L5_pyramidal nmda weight
+    net.connectivity[2]['nc_dict']['A_weight'] = 0.0004
+
+    # Modify L5_basket -> L5_pyramidal inhibition
+    net.connectivity[6]['nc_dict']['A_weight'] = 0.02  # gabaa
+    net.connectivity[7]['nc_dict']['A_weight'] = 0.005  # gabab
+
+    # Remove L5 pyramidal somatic and basal dendrite calcium channels
+    for sec in ['soma', 'basal_1', 'basal_2', 'basal_3']:
+        del net.cell_types['L5_pyramidal'].sections[
+            sec].mechs['ca']
+
+    # Remove L2_basket -> L5_pyramidal gabaa connection
+    del net.connectivity[10]  # Original paper simply sets gbar to 0.0
+
+    # Add L2_basket -> L5_pyramidal gabab connection
+    delay = net.delay
+    src_cell = 'L2_basket'
+    target_cell = 'L5_pyramidal'
+    lamtha = 50.
+    weight = 0.00005
+    loc = 'distal'
+    receptor = 'gabab'
+    net.add_connection(
+        src_cell, target_cell, loc, receptor, weight, delay, lamtha)
+
+    # Add L5_basket -> L5_pyramidal distal connection
+    # ("Martinotti-like recurrent tuft connection")
+    src_cell = 'L5_basket'
+    target_cell = 'L5_pyramidal'
+    lamtha = 70.
+    loc = 'distal'
+    receptor = 'gabaa'
+    key = f'gbar_L5Basket_L5Pyr_{receptor}'
+    weight = net._params[key]
+    net.add_connection(
+        src_cell, target_cell, loc, receptor, weight, delay, lamtha)
+
+    return net
